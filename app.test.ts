42122bbbe72077d369816f2e8dd4e2ffef434337
Galina Ostroumova <galina.ostroumova@ecs.co.uk>
2024-03-15 22:08:07 +0000

    added tets

--- app.test.ts
+++ app.test.ts
@@ -1,11 +1,44 @@
-const request = require("supertest");
-const app = require("./src/app");
+import request from "supertest";
+import app from "./src/app";
 
 describe("GET /properties", () => {
-  it("responds with a list of properties", async () => {
+  it("responds with status:200 and a list of properties", async () => {
     const response = await request(app).get("/properties");
 
     expect(response.statusCode).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body).toEqual([]);
+  });
+});
+
+describe("POST /properties", () => {
+  it("status:201 new property is added to list of properties", async () => {
+    const newProperty = {
+      id: 2,
+      address: "new address",
+      price: 500,
+      rating: 3,
+      image: "url",
+    };
+    const response = await request(app).post("/properties").send(newProperty);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual(newProperty);
+  });
+  it("returns 400 Bad Request when required fields are missing", async () => {
+    const malformedProperty = {
+      id: 3,
+      address: "new address",
+      price: "should_be_number",
+      rating: 3,
+      image: "url",
+    };
+
+    const response = await request(app)
+      .post("/properties")
+      .send(malformedProperty);
+
+    expect(response.statusCode).toBe(400);
+    // expect(response.error).toBe('"price" must be a number');
   });
 });
--- src/routes/properties.ts
+++ src/routes/properties.ts
@@ -1,15 +1,30 @@
 import { Router, Request, Response } from "express";
 import { Property } from "../models/Property";
+import Joi from "joi";
 
 let properties: Property[] = [];
 
 const router = Router();
+
+const propertySchema = Joi.object({
+  id: Joi.number().required(),
+  address: Joi.string().required(),
+  price: Joi.number().required(),
+  rating: Joi.number().required(),
+  image: Joi.string().required(),
+});
 
 router.get("/", (req: Request, res: Response) => {
   res.json(properties);
 });
 
 router.post("/", (req: Request, res: Response) => {
+  const { error } = propertySchema.validate(req.body);
+  if (error) {
+    console.log(error);
+
+    return res.status(400).send(error.details[0].message);
+  }
   const newProperty: Property = req.body;
   properties.push(newProperty);
   res.status(201).json(newProperty);
